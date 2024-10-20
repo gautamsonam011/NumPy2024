648477114513a3bf9015f8273571e1b2aee8d4a2
Sonam Gautam <gautamsonam011@gmail.com>
2024-10-20 18:51:27 +0530

    c

---
+++ .history/arrayjoin_20241020183351.py
@@ -0,0 +1,60 @@
+import numpy as np 
+
+arr1 = np.array([1, 2, 3, 4])
+
+arr2 = np.array([4, 5, 6, 7])
+
+arr = np.concatenate((arr1, arr2))
+
+print(arr)
+
+# 2-D 
+
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+
+arr = np.concatenate((arr1, arr2), axis=1)
+
+print(arr)
+
+# Joining arrays using stack function 
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.stack((arr1, arr2), axis = 1)
+
+print(arr)
+
+# Stcking along rows 
+
+# hstack()
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+arr = np.hstack((arr1, arr2))
+
+print(arr)
+
+# Stacking along columns 
+
+# vstack()
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.vstack((arr1, arr2))
+
+print(arr)
+
+# Stcking along height (depth) 
+
+# dstck()
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+arr = np.dstack((arr1, arr2))
+
+print(arr)
